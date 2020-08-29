582bdfc3dd472ff2aa170b8d1713bf8308839573
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-08-29 11:13:45 -0700

    set up connection

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize'); // sequelize
+require('dotenv').config();
+
+//connection to database
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'locahost',
+    dialect: 'mysql',
+    port: 3001
+});
+
+module.exports = sequelize;
