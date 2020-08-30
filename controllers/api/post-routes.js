ec0abaeb3b49840ef7e92f506ad0ae3a81c9d7b7
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-08-29 21:12:06 -0700

    added comment model to post route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,11 +1,19 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Comment } = require("../../models");
 
 // get route - all users
 router.get("/", (req, res) => {
   Post.findAll({
     attributes: ["id", "title", "body"],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_body", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
@@ -65,7 +73,7 @@ router.put("/:id", (req, res) => {
   Post.update(
     {
       title: req.body.title,
-      body: req.body.body
+      body: req.body.body,
     },
     {
       where: {
@@ -88,21 +96,21 @@ router.put("/:id", (req, res) => {
 
 //delete route - remove a post
 router.delete("/:id", (req, res) => {
-    Post.destroy({
-        where: {
-            id: req.params.id
-        }
+  Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
     })
-    .then(dbPostData => {
-        if(!dbPostData) {
-            res.status(404).json({ message: 'No post found with this id'});
-            return;
-        }
-        res.json(dbPostData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
