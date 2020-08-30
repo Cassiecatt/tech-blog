f9a35e8b5cc611e246c88bcdfaf292adc62f2bcd
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-08-30 13:32:31 -0700

    updated findall posts route for homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,18 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const { Post, Comment, User } = require("../models/");
 
 //get route - all posts on homepage
 router.get("/", (req, res) => {
-    Post.findAll({
-        include: [User],
-    })
+  Post.findAll({
+    attributes: ["id", "title", "body", "created_at"],
+    include: [User],
+  })
     .then((dbPostData) => {
-        const posts = dbPostData.map((post) => post.get({ plain: true }));
-        res.render("homepage", { posts });
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts });
     })
     .catch((err) => {
-        res.status(500).json(err);
+      res.status(500).json(err);
     });
 });
 
