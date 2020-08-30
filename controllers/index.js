96457671eea1e77ab73bbbec01d13bda5941bd30
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-08-30 12:10:43 -0700

    added stylesheet and html template through handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router(); // express
 
 const apiRoutes = require('./api'); // all api routes
+const homeRoutes = require('./home-routes'); // home routes
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const path = require('path');
 const express = require('express'); // require express
 const routes = require('./controllers'); // require all routes in folder
 const sequelize = require('./config/connection') // connection to server
+
+//handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001; // heroku
@@ -8,9 +13,14 @@ const PORT = process.env.PORT || 3001; // heroku
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public'))); // middleware that takes contents of a folder and serves them as static assets
+
 
 //turn on routes
 app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //turn on connection to db and server - .sync() means sequelize is taking the models and connection them to associate db tables
 sequelize.sync({ force: false }).then(() => {
