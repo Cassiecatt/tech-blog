15b2eba10ec134a9aaf31e94c5e9d72d01ca8810
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-08-30 13:06:56 -0700

    set up get route for all posts on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const { Post, Comment, User } = require("../models/");
 
+//get route - all posts on homepage
 router.get("/", (req, res) => {
-    res.render('homepage'); 
+    Post.findAll({
+        include: [User],
+    })
+    .then((dbPostData) => {
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+        res.render("homepage", { posts });
+    })
+    .catch((err) => {
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
